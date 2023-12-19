98f708c58d95bdf00b06ae92efbad6801b3398e2
jadamcrain <jadamcrain@gmail.com>
2023-12-19 10:40:20 -0800

    Add failing test case and certificates for verifying name from the CN field

--- tests/src/test_verifiers.rs
+++ tests/src/test_verifiers.rs
@@ -3,6 +3,7 @@ use std::convert::TryInto;
 use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
+use rustls::ServerConfig;
 use tokio::net::TcpStream;
 use tokio_rustls::rustls::pki_types::ServerName;
 use tokio_rustls::{TlsAcceptor, TlsConnector};
@@ -19,14 +20,19 @@ fn ca_san_dir() -> PathBuf {
     certs_dir().join("authority").join("san")
 }
 
-async fn connect() -> (tokio::net::TcpStream, tokio::net::TcpStream) {
+fn ca_subject_only_dir() -> PathBuf {
+    certs_dir().join("authority").join("subject_cn_only")
+}
+
+
+async fn connect() -> (TcpStream, TcpStream) {
     fn local_host(port: u16) -> SocketAddr {
         SocketAddrV4::new(Ipv4Addr::LOCALHOST, port).into()
     }
 
     let listener = tokio::net::TcpListener::bind(local_host(0)).await.unwrap();
     let assigned_port = listener.local_addr().unwrap().port();
-    let client = tokio::net::TcpStream::connect(local_host(assigned_port))
+    let client = TcpStream::connect(local_host(assigned_port))
         .await
         .unwrap();
     let (server, _) = listener.accept().await.unwrap();
@@ -34,17 +40,22 @@ async fn connect() -> (tokio::net::TcpStream, tokio::net::TcpStream) {
     (client, server)
 }
 
-#[tokio::test]
-async fn self_signed_verifier_works() {
-    let server_config = sfio_rustls_config::server::self_signed(
+fn get_self_signed_server_config() -> ServerConfig {
+    sfio_rustls_config::server::self_signed(
         MinProtocolVersion::V1_3,
         &self_signed_dir().join("bill.crt"),
         &self_signed_dir().join("jim.crt"),
         &self_signed_dir().join("jim.key"),
         None,
-    )
-    .unwrap();
+    ).unwrap()
+}
 
+#[tokio::test]
+async fn self_signed_verifier_works() {
+
+    let server_config = get_self_signed_server_config();
+
+    // this config pairs with the server config, i.e. jim and bill want to talk
     let client_config = sfio_rustls_config::client::self_signed(
         MinProtocolVersion::V1_3,
         &self_signed_dir().join("jim.crt"),
@@ -70,15 +81,10 @@ async fn self_signed_verifier_works() {
 
 #[tokio::test]
 async fn self_signed_verifier_rejects_wrong_cert() {
-    let server_config = sfio_rustls_config::server::self_signed(
-        MinProtocolVersion::V1_3,
-        &self_signed_dir().join("bill.crt"),
-        &self_signed_dir().join("jim.crt"),
-        &self_signed_dir().join("jim.key"),
-        None,
-    )
-    .unwrap();
 
+    let server_config = get_self_signed_server_config();
+
+    // this client config conflicts with the server config
     let client_config = sfio_rustls_config::client::self_signed(
         MinProtocolVersion::V1_3,
         &self_signed_dir().join("ted.crt"),
@@ -99,14 +105,15 @@ async fn self_signed_verifier_rejects_wrong_cert() {
     let name: ServerName = "whatever".try_into().unwrap();
 
     // both client and server should fail
-    let _client_err = connector.connect(name, client).await.unwrap_err();
-    let _server_err = server_task.await.unwrap().unwrap_err();
+    connector.connect(name, client).await.unwrap_err();
+    server_task.await.unwrap().unwrap_err();
 }
 
 #[tokio::test]
 async fn can_verify_server_name_in_san_extension() {
-    let (client, server) = client_can_verify_server_name(
+    let (client, server) = perform_ca_handshake(
         "server42",
+        ca_san_dir(),
         ClientNameVerification::None,
         ServerNameVerification::Verify,
     )
@@ -116,31 +123,47 @@ async fn can_verify_server_name_in_san_extension() {
 }
 
 #[tokio::test]
-async fn does_not_verify_server_name_in_common_name() {
-    let (client, server) = client_can_verify_server_name(
+async fn does_not_verify_server_name_in_common_name_when_san_is_present() {
+    let (client, server) = perform_ca_handshake(
         "myserver",
+        ca_san_dir(),
         ClientNameVerification::None,
         ServerNameVerification::Verify,
     )
     .await;
-    client.expect_err("client did NOT failed as expected");
-    server.expect_err("server did NOT failed as expected");
+    client.expect_err("client did NOT fail as expected");
+    server.expect_err("server did NOT fail as expected");
 }
+
+#[tokio::test]
+async fn can_verify_server_name_in_common_name_when_san_is_absent() {
+    let (client, server) = perform_ca_handshake(
+        "myserver",
+        ca_subject_only_dir(),
+        ClientNameVerification::None,
+        ServerNameVerification::Verify,
+    )
+    .await;
+    client.unwrap();
+    server.unwrap();
+}
+
 
 type ClientHandshakeResult = std::io::Result<tokio_rustls::client::TlsStream<TcpStream>>;
 type ServerHandshakeResult = std::io::Result<tokio_rustls::server::TlsStream<TcpStream>>;
 
-async fn client_can_verify_server_name(
+async fn perform_ca_handshake(
     name: &'static str,
+    path: PathBuf,
     client_name_verification: ClientNameVerification,
     server_name_verification: ServerNameVerification,
 ) -> (ClientHandshakeResult, ServerHandshakeResult) {
     let server_config = sfio_rustls_config::server::authority(
         MinProtocolVersion::V1_3,
         client_name_verification,
-        &ca_san_dir().join("ca.crt"),
-        &ca_san_dir().join("server.crt"),
-        &ca_san_dir().join("server.key"),
+        &path.join("ca.crt"),
+        &path.join("server.crt"),
+        &path.join("server.key"),
         None,
     )
     .unwrap();
@@ -148,9 +171,9 @@ async fn client_can_verify_server_name(
     let client_config = sfio_rustls_config::client::authority(
         MinProtocolVersion::V1_3,
         server_name_verification,
-        &ca_san_dir().join("ca.crt"),
-        &ca_san_dir().join("client.crt"),
-        &ca_san_dir().join("client.key"),
+        &path.join("ca.crt"),
+        &path.join("client.crt"),
+        &path.join("client.key"),
         None,
     )
     .unwrap();
