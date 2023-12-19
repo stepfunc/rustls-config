d00278d2ced5ef20a05162e3bba5dfd48a861535
jadamcrain <jadamcrain@gmail.com>
2023-12-19 08:46:57 -0800

    SAN specific test

--- tests/src/test_verifiers.rs
+++ tests/src/test_verifiers.rs
@@ -3,6 +3,7 @@ use std::convert::TryInto;
 use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
+use tokio::net::TcpStream;
 use tokio_rustls::rustls::pki_types::ServerName;
 use tokio_rustls::{TlsAcceptor, TlsConnector};
 
@@ -14,8 +15,8 @@ fn self_signed_dir() -> PathBuf {
     certs_dir().join("self-signed")
 }
 
-fn ca_dir() -> PathBuf {
-    certs_dir().join("authority")
+fn ca_san_dir() -> PathBuf {
+    certs_dir().join("authority").join("san")
 }
 
 async fn connect() -> (tokio::net::TcpStream, tokio::net::TcpStream) {
@@ -104,25 +105,42 @@ async fn self_signed_verifier_rejects_wrong_cert() {
 
 #[tokio::test]
 async fn can_verify_server_name_in_san_extension() {
-    client_can_verify_server_name("server42", ClientNameVerification::None, ServerNameVerification::Verify).await;
+    let (client, server) = client_can_verify_server_name(
+        "server42",
+        ClientNameVerification::None,
+        ServerNameVerification::Verify,
+    )
+    .await;
+    client.expect("client connection failed");
+    server.expect("server connection failed");
 }
 
 #[tokio::test]
-async fn can_verify_server_name_in_common_name() {
-    client_can_verify_server_name("myserver", ClientNameVerification::None, ServerNameVerification::Verify).await;
+async fn does_not_verify_server_name_in_common_name() {
+    let (client, server) = client_can_verify_server_name(
+        "myserver",
+        ClientNameVerification::None,
+        ServerNameVerification::Verify,
+    )
+    .await;
+    client.expect_err("client did NOT failed as expected");
+    server.expect_err("server did NOT failed as expected");
 }
+
+type ClientHandshakeResult = std::io::Result<tokio_rustls::client::TlsStream<TcpStream>>;
+type ServerHandshakeResult = std::io::Result<tokio_rustls::server::TlsStream<TcpStream>>;
 
 async fn client_can_verify_server_name(
     name: &'static str,
     client_name_verification: ClientNameVerification,
     server_name_verification: ServerNameVerification,
-) {
+) -> (ClientHandshakeResult, ServerHandshakeResult) {
     let server_config = sfio_rustls_config::server::authority(
         MinProtocolVersion::V1_3,
         client_name_verification,
-        &ca_dir().join("ca.crt"),
-        &ca_dir().join("server.crt"),
-        &ca_dir().join("server.key"),
+        &ca_san_dir().join("ca.crt"),
+        &ca_san_dir().join("server.crt"),
+        &ca_san_dir().join("server.key"),
         None,
     )
     .unwrap();
@@ -130,9 +148,9 @@ async fn client_can_verify_server_name(
     let client_config = sfio_rustls_config::client::authority(
         MinProtocolVersion::V1_3,
         server_name_verification,
-        &ca_dir().join("ca.crt"),
-        &ca_dir().join("client.crt"),
-        &ca_dir().join("client.key"),
+        &ca_san_dir().join("ca.crt"),
+        &ca_san_dir().join("client.crt"),
+        &ca_san_dir().join("client.key"),
         None,
     )
     .unwrap();
@@ -147,6 +165,8 @@ async fn client_can_verify_server_name(
     // doesn't matter what this is b/c it won't be checked
     let name: ServerName = name.try_into().unwrap();
 
-    let _client = connector.connect(name, client).await.unwrap();
-    let _server = server_task.await.unwrap();
+    let client = connector.connect(name, client).await;
+    let server = server_task.await.unwrap();
+
+    (client, server)
 }
