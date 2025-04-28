6b7cb86dccf1c9f242abcacf35a9c0c036387622
jadamcrain <jadamcrain@gmail.com>
2025-04-28 11:29:47 -0700

    rename feature

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,15 +1,15 @@
 #![doc = include_str!("../README.md")]
 
-#[cfg(all(feature = "crypto-ring", feature = "crypto-aws-lc-rs"))]
-compile_error!("Features 'crypto-ring' and 'crypto-aws-lc-rs' are mutually exclusive and cannot be enabled together");
+#[cfg(all(feature = "crypto-ring", feature = "crypto-aws-lc"))]
+compile_error!("Features 'crypto-ring' and 'crypto-aws-lc' are mutually exclusive and cannot be enabled together");
 
-#[cfg(not(any(feature = "crypto-ring", feature = "crypto-aws-lc-rs")))]
+#[cfg(not(any(feature = "crypto-ring", feature = "crypto-aws-lc")))]
 compile_error!("'crypto-ring' OR 'crypto-aws-lc-rs' must be enabled");
 
 #[cfg(feature = "crypto-ring")]
 pub(crate) use rustls::crypto::ring::default_provider as default_crypto_provider;
 
-#[cfg(feature = "crypto-aws-lc-rs")]
+#[cfg(feature = "crypto-aws-lc")]
 pub(crate) use rustls::crypto::aws_lc_rs::default_provider as default_crypto_provider;
 
 /// Client configurations
